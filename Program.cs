5170c779e3fed45cfac82d75923bcc91e38684c5
duyd486 <duyd486@gmail.com>
2024-05-04 16:33:28 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int []arr = { 2, 3, 4, 5,};
+        int x = int.Parse(Console.ReadLine());
+        for (int i = 0; i < arr.Length; i++) {
+            if (arr[i] == x) {
+                for (int j = i; j < arr.Length-1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length -1] = 0;
+            }
+        }
+        for (int i = 0; i < arr.Length; i++) {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
